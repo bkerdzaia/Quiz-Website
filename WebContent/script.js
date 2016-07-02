a7861e6fa4abe385982baa7cc41b6b7a5fa3f438
Baduri Kerdzaia <bkerd14@freeuni.edu.ge>
2016-07-02 13:23:59 +0400

    add cancel button for questions;

--- WebContent/script.js
+++ WebContent/script.js
@@ -4,26 +4,41 @@ $(function() {
 	$("#logOutId").load("logout.html");
 });
 
-/*  */
-function loadOrderDate() {
-	$(function(){
-		$("#performanceOrder").load("order-by-date.jsp");
-	});
+
+/* this functions makes a user to order user's past performance
+ * for specific quiz by date, percent correct, and amount of time */
+
+function showOrderByDate() {
+	$("#performanceOrderByDate").show();
+	$("#performanceOrderByPercentCorrect").hide();
+	$("#performanceOrderByAmountTime").hide();
 }
 
-/*  */
-$(function() {
-	$("input[name=performance]").click(function() {
-		// alert(this.value);
-		if (this.value === 'date') {
-			loadOrderDate();
-		} else {
-			$("#performanceOrder").empty();
-		}
-	});
-});
+function showOrderByPercentCorrect() {
+	$("#performanceOrderByDate").hide();
+	$("#performanceOrderByPercentCorrect").show();
+	$("#performanceOrderByAmountTime").hide();
+}
+
+function showOrderByAmountTime() {
+	$("#performanceOrderByDate").hide();
+	$("#performanceOrderByPercentCorrect").hide();
+	$("#performanceOrderByAmountTime").show();
+}
+
+
 
 /** this function is for adding questions and answers */
+
+/* variables for counting question types added number */
+var fillInTheBlankQuestionNumber = 0;
+var pictureResponseQuestionNumber = 0;
+var questionResponseTypeNumber = 0;
+var multipleChoiceTypeNumber = 0;
+
+var questionNumber = 0;
+
+
 /* return concatenation of className and number */
 function toName(className, number) {
 	return className + number;
@@ -40,30 +55,34 @@ function addPossibleAnswer(className) {
 			getInputTextTag(className));
 }
 
+function getAddAnswerButton(className) {
+	return  "<button type=\"button\" id=\"addPossibleAnswerButtonId" + className + 
+			"\" onclick=\"addPossibleAnswer(" + "'" + className + "')\">+ Add Another answer</button>";
+}
+
 /* gets questions body for the type of question
  * which has one question and may be have multiple answers */
 function getQuestionAnswers(parantClassName, idNumber) {
 	var className = toName(parantClassName, idNumber);
 	return "Enter Question Here: " + getInputTextTag(className) +
-			"Enter Possible Answer: " + getInputTextTag(className) +
-			"<button type=\"button\" id=\"addPossibleAnswerButtonId" + className + 
-			"\" onclick=\"addPossibleAnswer(" + "'" + className + "')\">+ Add Another answer</button><br>";
+			"Enter Possible Answer: " + getInputTextTag(className) + getAddAnswerButton(className) + getCancelButton();
 }
 
 function getPictureResponseQuestion(parantClassName, idNumber) {
 	var className = toName(parantClassName, idNumber);
 	return "Enter Question Here: " + getInputTextTag(className) +
 			"Enter URL Here: " + getInputTextTag(className) +
-			"Enter Possible Answer: " + getInputTextTag(className) +
-			"<button type=\"button\" id=\"addPossibleAnswerButtonId" + className + 
-			"\" onclick=\"addPossibleAnswer(" + "'" + className + "')\">+ Add Another answer</button><br>";
+			"Enter Possible Answer: " + getInputTextTag(className) + getAddAnswerButton(className) + getCancelButton();
 }
 
-/* variables for counting question types added number */
-var fillInTheBlankQuestionNumber = 0;
-var pictureResponseQuestionNumber = 0;
-var questionResponseTypeNumber = 0;
-var multipleChoiceTypeNumber = 0;
+
+function removeQuestion(questionNom) {
+	$("#questionNumber" + questionNom).remove();
+}
+
+function getCancelButton() {
+	return  "<button type=\"button\" onclick=\"removeQuestion(" + questionNumber + ")\">Cancel</button><br>";
+}
 
 /* add question function */
 $(function() {
@@ -72,23 +91,32 @@ $(function() {
 		if (questionTypeValue === "MultipleChoiceType") {
 			multipleChoiceTypeNumber++;
 			var className = toName("multipleChoiceQuestion", multipleChoiceTypeNumber);
-			$("#addQuestionsType").prepend("<div class=\"multipleChoiceQuestion\"> " +
+			$("#addQuestionsType").prepend("<div class=\"multipleChoiceQuestion\" id='questionNumber" + questionNumber + "'> "
+					+ "<p><b>" + questionTypeValue + ": </b></p> " +
 					"Enter Question Here: " + getInputTextTag(className) +
 					"Enter Correct Answer: " + getInputTextTag(className) +
 					"Enter Possible Answer: " + getInputTextTag(className) +
 					"Enter Possible Answer: " + getInputTextTag(className) +
 					"Enter Possible Answer: " + getInputTextTag(className) +
-					"</div><br>");	
+					getCancelButton() + "<br></div>");	
 		} else if(questionTypeValue === "FillInTheBlankType") {
 			fillInTheBlankQuestionNumber++;
-			$("#addQuestionsType").prepend("<div  class=\"fillInTheBlankQuestion\">" + getQuestionAnswers("fillInTheBlankQuestion", fillInTheBlankQuestionNumber) +"</div>");
+			$("#addQuestionsType").prepend("<div  class=\"fillInTheBlankQuestion\" id='questionNumber" + questionNumber + "'> "
+					+ "<p><b>" + questionTypeValue + ": </b></p> "  
+					+ getQuestionAnswers("fillInTheBlankQuestion", fillInTheBlankQuestionNumber) +"</div>");
 		} else if(questionTypeValue === "QuestionResponseType") {
 			questionResponseTypeNumber++;
-			$("#addQuestionsType").prepend("<div  class=\"questionResponseQuestion\">" + getQuestionAnswers("questionResponseQuestion", questionResponseTypeNumber) +"</div>");
+			var className = toName("questionResponseQuestion", questionResponseTypeNumber);
+			$("#addQuestionsType").prepend("<div  class=\"questionResponseQuestion\" id='questionNumber" + questionNumber + "'> "
+					+ "<p><b>" + questionTypeValue + ": </b></p> "  
+					+ getQuestionAnswers("questionResponseQuestion", questionResponseTypeNumber) +"</div>");
+			addPossibleAnswer(className);
 		} else if(questionTypeValue === "PictureResponseType") {
 			pictureResponseQuestionNumber++;
-			$("#addQuestionsType").prepend("<div  class=\"pictureResponseQuestion\">" + getPictureResponseQuestion("pictureResponseQuestion", pictureResponseQuestionNumber) +"</div>");
+			$("#addQuestionsType").prepend("<div  class=\"pictureResponseQuestion\" id='questionNumber" + questionNumber + "'> "
+					+ "<p><b>" + questionTypeValue + ": </b></p> "  
+					+ getPictureResponseQuestion("pictureResponseQuestion", pictureResponseQuestionNumber) +"</div>");
 		}
-		$("#addQuestionsType").prepend("<p><b>" + questionTypeValue + ": </b></p>");
+		questionNumber++;
 	});
 });
