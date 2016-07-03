4ff64ba3701fa994125eda7a0acf37ea032e9d51
aleksandreK <alkakh14@freeuni.edu.ge>
2016-07-03 17:25:20 +0400

    updated quiz.jsp to be as short as possible

--- src/quiz/QuizHistory.java
+++ src/quiz/QuizHistory.java
@@ -1,14 +1,24 @@
 package quiz;
 
-import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @SuppressWarnings("serial")
 public class QuizHistory extends History<PerformanceOnQuiz> {
 
-	public ArrayList<PerformanceOnQuiz> sortByUser(String userName, Comparator<Performance> comparator) {
-		return (ArrayList<PerformanceOnQuiz>) this.stream()
+	/**
+	 * Sorts the PerformanceOnQuiz by comparator for the user's whose name equal
+	 * to userName.
+	 * 
+	 * @param userName
+	 *            - the name of user whose performance should be sorted.
+	 * @param comparator
+	 *            - the comparator of Performance for sorting
+	 * @return list of sorted PerformanceOnQuiz of given user's name
+	 */
+	public List<PerformanceOnQuiz> sortByUser(String userName, Comparator<Performance> comparator) {
+		return this.stream()
 				.filter(quizPerformance -> quizPerformance.getUser().equals(userName))
 					.sorted(comparator)
 						.collect(Collectors.toList());
